cf1a4bb5c99f905678bb1b3652ecafb453cb2332
Janis Bürkle <bks@teamtechnik.com>
2023-12-14 11:32:44 +0100

    First

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,54 @@
 ﻿using System;
 using System.Net.Http;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ServerStart
 {
     public partial class MainWindow : Window
     {
+        private const string ServerUrl = "http://localhost:8080/startMinecraftServer";
+        private string parameterValue;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void StartServerButtonClick(object sender, RoutedEventArgs e)
         {
-            using (HttpClient client = new HttpClient())
+            parameterValue = "startServer";
+            RequestAndHandleServerAction();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+        private void StopServerButtonClick(object sender, RoutedEventArgs e)
+        {
+            parameterValue = "stopServer";
+            RequestAndHandleServerAction();
+        }
+
+        private async void RequestAndHandleServerAction()
+        {
+            using (var client = new HttpClient())
             {
-                string serverUrl = "http://localhost:8080/startMinecraftServer";
+                var serverUrlWithParameter = $"{ServerUrl}?parameter={parameterValue}";
+
                 try
                 {
-                    HttpResponseMessage response = client.GetAsync(serverUrl).Result;
+                    var response = await client.GetAsync(serverUrlWithParameter);
 
                     if (response.IsSuccessStatusCode)
                     {
-                        // Lese die Serverantwort
-                        string serverResponse = response.Content.ReadAsStringAsync().Result;
-
-                        // Zeige die Serverantwort in einer MessageBox an
+                        var serverResponse = response.Content.ReadAsStringAsync().Result;
                         MessageBox.Show(serverResponse, "Serverantwort");
                     }
                     else
                     {
-                        MessageBox.Show("Fehler beim Starten des Servers.");
+                        MessageBox.Show("Fehler beim Ausführen der Aktion auf dem Server.");
                     }
                 }
                 catch (Exception ex)
@@ -38,6 +56,11 @@ namespace ServerStart
                     MessageBox.Show($"Fehler: {ex.Message}");
                 }
             }
+        }
+
+        private void ExitButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
